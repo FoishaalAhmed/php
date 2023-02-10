c2c1da98010fe350cf80da690c61188fa3d63564
foisal_techvill <foisal.techvill@gmail.com>
2023-02-10 23:09:24 +0600

    initial commit

---
+++ cookie.php
@@ -0,0 +1,8 @@
+<?php
+echo "# php" >> README.md
+git init
+ README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/FoishaalAhmed/php.git
+git push -u origin main
